227e9978981937346c3d1991fec55bd2b4055269
antsiri <antoniosirignano@outlook.it>
2024-06-03 21:43:00 +0200

    first result

--- Code/ray_tracing_1.py
+++ Code/ray_tracing_1.py
@@ -1,3 +1,4 @@
+
 import numpy as np
 import matplotlib.pyplot as plt
 
@@ -5,9 +6,9 @@ def normalization(vector):
     return vector / np.linalg.norm(vector)
 
 def sphere_intersect(center, radius, ray_origin, ray_direction):
-    b = 2* np.dot(ray_direction, ray_origin - center)
+    b = 2 * np.dot(ray_direction, ray_origin - center)
     c = np.linalg.norm(ray_origin - center) ** 2 - radius ** 2
-    delta = b**2 - 4*c
+    delta = b ** 2 - 4 * c
     if delta > 0:
         t1 = (-b + np.sqrt(delta)) / 2
         t2 = (-b - np.sqrt(delta)) / 2
@@ -25,18 +26,21 @@ def nearest_intersected_object(objects, ray_origin, ray_direction):
             nearest_object = objects[index]
     return nearest_object, min_distance
 
-width = 500
-height = 300
+width = 300
+height = 200
+
+max_depth = 3
 
 camera = np.array([0, 0, 1])
 ratio = float(width) / height
-#sinistra, sopra, destra, sotto
-screen = (-1, 1/ratio, 1, -1/ratio)  
+screen = (-1, 1 / ratio, 1, -1 / ratio)
+
+light = { 'position': np.array([5, 5, 5]), 'ambient': np.array([1, 1, 1]), 'diffuse': np.array([1, 1, 1]), 'specular': np.array([1, 1, 1]) }
 
 objects = [
-    {'center': np.array([-0.2, 0, -1]), 'radius':0.7},
-    {'center': np.array([0.1, -0.3, 0]), 'radius':0.1},
-    {'center': np.array([-0.3, 0, 0]), 'radius':0.15}
+    { 'center': np.array([-0.2, 0, -1]), 'radius': 0.7, 'ambient': np.array([0.1, 0, 0]), 'diffuse': np.array([0.7, 0, 0]), 'specular': np.array([1, 1, 1]), 'shininess': 100, 'reflection': 0.5 },
+    { 'center': np.array([0.1, -0.3, 0]), 'radius': 0.1, 'ambient': np.array([0.1, 0, 0.1]), 'diffuse': np.array([0.7, 0, 0.7]), 'specular': np.array([1, 1, 1]), 'shininess': 100, 'reflection': 0.5 },
+    { 'center': np.array([-0.3, 0, 0]), 'radius': 0.15, 'ambient': np.array([0, 0.1, 0]), 'diffuse': np.array([0, 0.6, 0]), 'specular': np.array([1, 1, 1]), 'shininess': 100, 'reflection': 0.5 }
 ]
 
 image = np.zeros((height, width, 3))
@@ -52,10 +56,36 @@ for i, y in enumerate(np.linspace(screen[1], screen[3], height)):
             continue
 
         # compute intersection point between ray and nearest object
-        intersection = origin + min_distance*direction
+        intersection = origin + min_distance * direction
+
+        normal_to_surface = normalization(intersection - nearest_object['center'])
+        shifted_point = intersection + 1e-5 * normal_to_surface
+        intersection_to_light = normalization(light['position'] - shifted_point)
+
+        _, min_distance = nearest_intersected_object(objects, shifted_point, intersection_to_light)
+        intersection_to_light_distance = np.linalg.norm(light['position'] - intersection)
+        is_shadowed = min_distance < intersection_to_light_distance
         
-    # image[i, j] = ...  
-    print("progress: %d/%d" % (i+1, height))
+        if is_shadowed:
+            continue
+
+        # RGB
+        illumination = np.zeros((3))
+
+        # ambiant
+        illumination += nearest_object['ambient'] * light['ambient']
+
+        # diffuse
+        illumination += nearest_object['diffuse'] * light['diffuse'] * np.dot(intersection_to_light, normal_to_surface)
+
+        # specular
+        intersection_to_camera = normalization(camera - intersection)
+        H = normalization(intersection_to_light + intersection_to_camera)
+        illumination += nearest_object['specular'] * light['specular'] * np.dot(normal_to_surface, H) ** (nearest_object['shininess'] / 4)
+
+        image[i, j] = np.clip(illumination, 0, 1)
+  
+    #print("progress: %d/%d" % (i+1, height))
 
 plt.imsave('image.png', image)
 
